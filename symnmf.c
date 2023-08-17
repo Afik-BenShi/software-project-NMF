0ad2a7384cfc8a8b4604d55104f14f2900f9343b
Afik Ben Shimol <afiks@wix.com>
2023-08-17 10:18:27 +0300

    Attempt at solution

--- symnmf.c
+++ symnmf.c
@@ -5,7 +5,7 @@
 
 /* Calculates the similarity matrix from the points
  * according to sec 1.1 in the pdf*/
-double** sym(double **points, int line_num, int dim){
+double **sym(double **points, int line_num, int dim){
     int i,j;
     double **A;
     A = (double **)malloc(line_num*sizeof(double *));
@@ -48,19 +48,76 @@ double **ddg(double **sym_mat, int line_num){
 }
 
 
+double **norm(double **sym_mat, double **ddg_mat, int line_num){
+    int i, j;
+    double *inv_sqrt_ddg, **normalized_sym;
+    inv_sqrt_ddg = (double *)malloc(line_num * sizeof(double));
+    pointer_check((void *)inv_sqrt_ddg, GENERAL_ERROR);
+
+    /* calculate ddg^-0.5 (only the diagonal => 1D)*/
+    for (i = 0; i < line_num; i++)
+    {
+        inv_sqrt_ddg[i] = -1/sqrt(ddg_mat[i][i]);
+    }
+
+    /* multiply matricies */
+    normalized_sym = (double *)malloc(line_num * sizeof(double));
+    pointer_check((void *)normalized_sym, GENERAL_ERROR);
+    for (i = 0; i < line_num; i++)
+    {
+        normalized_sym[i] = (double *)malloc(line_num * sizeof(double));
+        pointer_check((void *)normalized_sym[i], GENERAL_ERROR);
+        for (j=0; j< line_num; j++){
+            normalized_sym[i][j] = inv_sqrt_ddg[i]*sym_mat[i][j]*inv_sqrt_ddg[j];
+        }
+    }
+    free(inv_sqrt_ddg);
+    return normalized_sym;
+}
+
+/**
+ * prints a 2D matrix of doubles
+*/
+void matrix_printer(double **matrix, int rows, int cols){
+    return;
+}
 
 int main(int argc, char *argv)
 {
-    int i, j;
+    int i, j, dim, line_num;
     char *goal = argv[1], *filename = argv[2];
-    if (goal == "sym")
-    {
+    double **points, **sym_mat, **ddg_mat, **result;
+    char *file_buffer, **lines;
+    
+    /* file reading and parsing to points*/
+    file_buffer = read_file(filename); 
+    dim = get_dimention(file_buffer);
+    line_num = count_lines(file_buffer);
+    lines = split_to_lines(file_buffer, line_num);
+    points = lines_to_points(lines, line_num, dim);
+
+    /* free up memory */
+    free_2d(lines, line_num);
+    /* don't need to free file_buffer, becase lines point to the same location */
+
+    sym_mat = sym(points, line_num, dim);
+    if (goal == "sym"){
+        result = sym_mat;
     }
-    else if (goal == "ddg")
+    if (goal == "ddg")
     {
+        result = ddg(sym_mat, line_num);
+        free_2d(sym_mat, line_num);
     }
-    else if (goal == "norm")
+    if (goal == "norm")
     {
+        ddg_mat = ddg(sym_mat, line_num);
+        result = norm(sym_mat, ddg_mat, line_num);
+        free_2d(sym_mat, line_num);
+        free_2d(ddg_mat, line_num);
     }
+    /* print and free all memory */
+    matrix_printer(result, line_num, line_num);
+    free_2d(result, line_num);
     return 1;
 }
